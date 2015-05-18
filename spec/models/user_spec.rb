2fdce676c6c1fb54b14067b4225c150e0e28871b
Yuta Tokuda <yuta.tokuda@luxiar.com>
2015-05-18 10:05:30 +0900

    signout

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,9 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-
+  it { should respond_to(:authenticate) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   describe "when name is not present" do
@@ -111,5 +113,9 @@ describe User do
       @user.save
       expect(@user.reload.email).to eq mixed_case_email.downcase
     end
+  end
+    describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
