70eced77497729720393887da29e713304609dbc
Yuta Tokuda <yuta.tokuda@luxiar.com>
2015-05-19 11:06:27 +0900

    aaa

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -32,14 +32,10 @@ describe "Authentication" do
       before { sign_in user }
 
       it { should have_title(user.name) }
+      it { should have_link('Users',       href: users_path) }
       it { should have_link('Profile',     href: user_path(user)) }
       it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
-      it { should_not have_link('Sign in', href: signin_path) }
-
-      it { should have_title(user.name) }
-      it { should have_link('Profile', href: user_path(user)) }
-      it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
       describe "followed by signout" do
         before { click_link "Sign out" }
@@ -80,23 +76,29 @@ describe "Authentication" do
           before { patch user_path(user) }
           specify { expect(response).to redirect_to(signin_path) }
         end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_title('Sign in') }
+        end
       end
-    end
+    
 
-    describe "as wrong user" do
-      let(:user) { FactoryGirl.create(:user) }
-      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
-      before { sign_in user, no_capybara: true }
+      describe "as wrong user" do
+          let(:user) { FactoryGirl.create(:user) }
+          let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+        before { sign_in user, no_capybara: true }
 
-      describe "submitting a GET request to the Users#edit action" do
-        before { get edit_user_path(wrong_user) }
-        specify { expect(response.body).not_to match(full_title('Edit user')) }
-        specify { expect(response).to redirect_to(root_url) }
-      end
+          describe "submitting a GET request to the Users#edit action" do
+            before { get edit_user_path(wrong_user) }
+            specify { expect(response.body).not_to match(full_title('Edit user')) }
+            specify { expect(response).to redirect_to(root_url) }
+          end
 
-      describe "submitting a PATCH request to the Users#update action" do
-        before { patch user_path(wrong_user) }
-        specify { expect(response).to redirect_to(root_path) }
+        describe "submitting a PATCH request to the Users#update action" do
+          before { patch user_path(wrong_user) }
+          specify { expect(response).to redirect_to(root_path) }
+        end
       end
     end
   end
