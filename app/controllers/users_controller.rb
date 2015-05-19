bc9f8d4b5237bffe4684d5d4e9f9f5e20c5f8fb1
Yuta Tokuda <yuta.tokuda@luxiar.com>
2015-05-19 09:54:23 +0900

    totyu

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,25 @@ class UsersController < ApplicationController
     def new
     @user = User.new
     end
+
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+   def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
    def create
     @user = User.new(user_params)
     if @user.save
