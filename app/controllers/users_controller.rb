0bc358fa474b16a44fbf949eba249ddbc762ccd8
Yuta Tokuda <yuta.tokuda@luxiar.com>
2015-05-20 14:58:02 +0900

    aaa

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,8 +23,9 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page])
   end
 
-  def show
+   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def edit
